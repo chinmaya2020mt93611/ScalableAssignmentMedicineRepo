e0600b105013ac3cfb3b590af7dedfae5c1ee054
chinmaya2020mt93611 <2020mt93611@wilp.bits-pilani.ac.in>
2022-04-14 08:35:37 +0530

    first commit

---
+++ src/main/java/com/scalable/assignment/AssignmentController.java
@@ -0,0 +1,71 @@
+package com.scalable.assignment;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AssignmentController {
+	
+	@Autowired
+    private JdbcTemplate jdbcTemplate;
+
+	@GetMapping("/medicine/getDetails")
+	public String index() {
+		String sql = "SELECT * FROM MEDICINE";
+        
+		List<Map<String, Object>> medicines = jdbcTemplate.queryForList(sql);
+		StringBuilder sb = new StringBuilder();
+		
+		if (medicines!=null && !medicines.isEmpty()) {
+			
+			for (Map<String, Object> medicine : medicines) {
+				
+				for (Iterator<Map.Entry<String, Object>> it = medicine.entrySet().iterator(); it.hasNext();) {
+					Map.Entry<String, Object> entry = it.next();
+					String key = entry.getKey();
+					Object value = entry.getValue();
+					sb.append(key + " = " + value + "<br>");
+				}
+				sb.append("<br>");
+				sb.append("------------------------------");
+				sb.append("<br>");
+			}
+			
+		}
+		return sb.toString();
+	}
+	
+	//CREATE TABLE COMPANY(PatientId INT PRIMARY KEY NOT NULL, NAME TEXT NOT NULL, AGE TEXT NOT NULL);
+	
+	
+	  @GetMapping("/createTable") public void tableCreate() { 
+		  String sql = "CREATE TABLE MEDICINE(MedicineId TEXT PRIMARY KEY NOT NULL, MedicineName TEXT NOT NULL, MedicineExpiry TEXT NOT NULL)";
+	  
+		  jdbcTemplate.execute(sql); 
+	  }
+	 
+	
+	// API: POST - /add/patientDetails
+	@PostMapping("/add/medicineDetails")
+	public void addPatient(@RequestBody MedicineRecord request, HttpServletResponse response) {
+		String sql = "INSERT INTO MEDICINE VALUES (?, ?, ?)";
+		jdbcTemplate.update(sql, request.medicineId, request.medicineName, request.medicineExpiry);
+	}
+	
+	@PostMapping("/fetch/patientDrugDetails")
+	public void fetchDrug(String patientid) {
+		
+	}
+
+}
---
+++ src/main/java/com/scalable/assignment/MedicineRecord.java
@@ -0,0 +1,26 @@
+package com.scalable.assignment;
+
+public class MedicineRecord {
+	String medicineId;
+	String medicineName;
+	String medicineExpiry;
+	public String getMedicineId() {
+		return medicineId;
+	}
+	public void setMedicineId(String medicineId) {
+		this.medicineId = medicineId;
+	}
+	public String getMedicineName() {
+		return medicineName;
+	}
+	public void setMedicineName(String medicineName) {
+		this.medicineName = medicineName;
+	}
+	public String getMedicineExpiry() {
+		return medicineExpiry;
+	}
+	public void setMedicineExpiry(String medicineExpiry) {
+		this.medicineExpiry = medicineExpiry;
+	}
+	
+}
